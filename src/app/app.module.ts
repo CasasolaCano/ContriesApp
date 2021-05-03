8e687d7f698f39f753dc08d8a782e99d395006c8
CasasolaCano <casasolacontact@gmail.com>
2021-05-03 14:12:00 +0200

    Country service get countries by name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { SharedModule } from './shared/shared.module';
 import { CountryModule } from './country/country.module';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     SharedModule,
     CountryModule
   ],
