364cbd8529915197a6953730692d63a33978ef26
CasasolaCano <casasolacontact@gmail.com>
2021-05-10 13:35:48 +0200

    starting by-region template

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
   styleUrls: ['./by-region.component.css']
 })
-export class ByRegionComponent implements OnInit {
+export class ByRegionComponent {
+
+  regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+
+  activeRegion: string = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+  activateRegion (region:string) {
+    this.activeRegion = region;
   }
 
 }
